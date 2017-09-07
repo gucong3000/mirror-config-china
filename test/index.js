d6aefb2cb066c952710cde28e46809bfbc3e67fd
刘祺 <gucong@gmail.com>
2017-09-07 16:48:56 +0800

    Environment variables (#13)
    * Support environment variables
    
    * testcase
    
    * debug ci config
    
    * debug ci config
    
    * debug
    
    * skip testcase of environment
    
    * update appveyor config

--- test/index.js
+++ test/index.js
@@ -3,8 +3,16 @@ var describe = require('mocha').describe;
 var it = require('mocha').it;
 var assert = require('assert');
 
-describe('env', function() {
+describe.skip('environment variables', function() {
+	it('electron', function() {
+		assert.ok(process.env.ELECTRON_MIRROR);
+	});
+	it('node', function() {
+		assert.ok(process.env.NODEJS_ORG_MIRROR);
+	});
+});
 
+describe('npm config', function() {
 	it('electron', function() {
 		assert.ok(process.env.npm_config_electron_mirror);
 	});
