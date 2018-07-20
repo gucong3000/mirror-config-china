ab4a31de609f501fea17c73a880dea77f67ab17b
刘祺 <gucong@gmail.com>
2018-07-20 12:06:28 +0800

    update eslint config (#48)

--- test/index.js
+++ test/index.js
@@ -78,84 +78,84 @@ describe('environment variables', () => {
 	});
 
 	it('NODEJS_ORG_MIRROR', () => {
-		assert.equal(process.env.NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
+		assert.strictEqual(process.env.NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
 	});
 
 	it('NVM_NODEJS_ORG_MIRROR', () => {
-		assert.equal(process.env.NVM_NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
+		assert.strictEqual(process.env.NVM_NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
 	});
 
 	it('IOJS_ORG_MIRROR', () => {
-		assert.equal(process.env.IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
+		assert.strictEqual(process.env.IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
 	});
 
 	it('NVM_IOJS_ORG_MIRROR', () => {
-		assert.equal(process.env.NVM_IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
+		assert.strictEqual(process.env.NVM_IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
 	});
 
 	it('NVMW_NODEJS_ORG_MIRROR', () => {
-		assert.equal(process.env.NVMW_NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
+		assert.strictEqual(process.env.NVMW_NODEJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/node');
 	});
 	it('NVMW_IOJS_ORG_MIRROR', () => {
-		assert.equal(process.env.NVMW_IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
+		assert.strictEqual(process.env.NVMW_IOJS_ORG_MIRROR, 'https://npm.taobao.org/mirrors/iojs');
 	});
 
 	it('NVMW_NPM_MIRROR', () => {
-		assert.equal(process.env.NVMW_NPM_MIRROR, 'https://npm.taobao.org/mirrors/npm');
+		assert.strictEqual(process.env.NVMW_NPM_MIRROR, 'https://npm.taobao.org/mirrors/npm');
 	});
 });
 
 describe('npm config', () => {
 	it('chromedriver', () => {
-		assert.equal(process.env.npm_config_chromedriver_cdnurl, 'https://npm.taobao.org/mirrors/chromedriver');
+		assert.strictEqual(process.env.npm_config_chromedriver_cdnurl, 'https://npm.taobao.org/mirrors/chromedriver');
 	});
 
 	it('electron', () => {
-		assert.equal(process.env.npm_config_electron_mirror, 'https://npm.taobao.org/mirrors/electron/');
+		assert.strictEqual(process.env.npm_config_electron_mirror, 'https://npm.taobao.org/mirrors/electron/');
 	});
 
 	it('git4win', () => {
-		assert.equal(process.env.npm_config_git4win_mirror, 'https://npm.taobao.org/mirrors/git-for-windows');
+		assert.strictEqual(process.env.npm_config_git4win_mirror, 'https://npm.taobao.org/mirrors/git-for-windows');
 	});
 
 	it('node-inspector', () => {
-		assert.equal(process.env.npm_config_profiler_binary_host_mirror, 'https://npm.taobao.org/mirrors/node-inspector/');
+		assert.strictEqual(process.env.npm_config_profiler_binary_host_mirror, 'https://npm.taobao.org/mirrors/node-inspector/');
 	});
 
 	it('node-sass', () => {
-		assert.equal(process.env.npm_config_sass_binary_site, 'https://npm.taobao.org/mirrors/node-sass');
+		assert.strictEqual(process.env.npm_config_sass_binary_site, 'https://npm.taobao.org/mirrors/node-sass');
 	});
 
 	it('nodegit', () => {
-		assert.equal(process.env.npm_config_nodegit_binary_host_mirror, 'https://npm.taobao.org/mirrors/nodegit/v{version}/');
+		assert.strictEqual(process.env.npm_config_nodegit_binary_host_mirror, 'https://npm.taobao.org/mirrors/nodegit/v{version}/');
 	});
 
 	it('operadriver', () => {
-		assert.equal(process.env.npm_config_operadriver_cdnurl, 'https://npm.taobao.org/mirrors/operadriver');
+		assert.strictEqual(process.env.npm_config_operadriver_cdnurl, 'https://npm.taobao.org/mirrors/operadriver');
 	});
 
 	it('phantomjs', () => {
-		assert.equal(process.env.npm_config_phantomjs_cdnurl, 'https://npm.taobao.org/mirrors/phantomjs');
+		assert.strictEqual(process.env.npm_config_phantomjs_cdnurl, 'https://npm.taobao.org/mirrors/phantomjs');
 	});
 
 	it('puppeteer', () => {
-		assert.equal(process.env.npm_config_puppeteer_download_host, 'https://npm.taobao.org/mirrors');
+		assert.strictEqual(process.env.npm_config_puppeteer_download_host, 'https://npm.taobao.org/mirrors');
 	});
 
 	it('python', () => {
-		assert.equal(process.env.npm_config_python_mirror, 'https://npm.taobao.org/mirrors/python');
+		assert.strictEqual(process.env.npm_config_python_mirror, 'https://npm.taobao.org/mirrors/python');
 	});
 
 	it('sqlite3', () => {
-		assert.equal(process.env.npm_config_sqlite3_binary_site, 'https://npm.taobao.org/mirrors/sqlite3');
+		assert.strictEqual(process.env.npm_config_sqlite3_binary_site, 'https://npm.taobao.org/mirrors/sqlite3');
 	});
 });
 
 describe('get config', () => {
 	it('--registry', () => {
-		assert.equal(config(['--registry=https://r.cnpmjs.org']).registry, 'https://r.cnpmjs.org');
+		assert.strictEqual(config(['--registry=https://r.cnpmjs.org']).registry, 'https://r.cnpmjs.org');
 	});
 	it('--disturl', () => {
-		assert.equal(config(['--disturl=https://cnpmjs.org/dist']).disturl, 'https://cnpmjs.org/dist');
+		assert.strictEqual(config(['--disturl=https://cnpmjs.org/dist']).disturl, 'https://cnpmjs.org/dist');
 	});
 });
